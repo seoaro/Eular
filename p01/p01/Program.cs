885d38cb361e4d90eef1627aadb3fa4c31f5373a
Juhyung Seo <seoaro@gmail.com>
2016-03-11 14:16:56 +0900

    Problem 1 10보다 작은 자연수 중에서 3 또는 5의 배수는 3, 5, 6, 9 이고, 이것을 모두 더하면 23입니다.
    1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면 얼마일까요?

--- p01/p01/Program.cs
+++ p01/p01/Program.cs
@@ -10,6 +10,45 @@ namespace p01
     {
         static void Main(string[] args)
         {
+            int dividedByThree = Remainder(1000, 3);
+            int dividedByFive = Remainder(1000, 5);
+            
+            int[] numThree = new int[dividedByThree];
+            int[] numFive = new int[dividedByFive];
+            int i = 0;
+            int j = 0;
+            int sum = 0;
+            while(i < dividedByThree)
+            {
+                numThree[i] = (i + 1) * 3;
+                sum = sum + numThree[i];
+                i++;
+            }
+
+            while(j < dividedByFive)
+            {
+                numFive[j] = (j + 1) * 5;
+                sum = sum + numFive[j];
+                j++;
+            }
+            Console.WriteLine("1000 has {0} 3s, {1} 5s and sum of 3s & 5s is {2}", dividedByThree, dividedByFive, sum);
+        }
+
+        static int Remainder(int num1, int num2)
+        {
+            int result;
+            int resultTemp;
+            resultTemp = num1 % num2;
+
+            if(resultTemp == 0)
+            {
+                result = num1 / num2 - 1;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+            return result;
         }
     }
 }
