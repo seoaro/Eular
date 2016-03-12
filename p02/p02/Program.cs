e65e6bd27456895f7852009dc7f098729f429455
Juhyung Seo <seoaro@gmail.com>
2016-03-12 16:06:34 +0900

    Problem 2 피보나치 수열의 각 항은 바로 앞의 항 두 개를 더한 것이 됩니다. 1과 2로 시작하는 경우 이 수열은 아래와 같습니다.
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    짝수이면서 4백만 이하인 모든 항을 더하면 얼마가 됩니까?

---
+++ p02/p02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int one = 1;
+            int two = 2;
+            int three = 3;
+
+            int fiboNum = 0;
+            int sum = 0;
+            while(fiboNum <= 4000000)
+            {
+                three = one + two;
+                one = two;
+                two = three;
+                fiboNum = two - one;
+                i++;
+                if(fiboNum%2 == 0)
+                {
+                    sum = sum + fiboNum;
+                }
+                Console.WriteLine("i = {0} : {1}, {2}, {3}", i, fiboNum, one, two);
+
+            }
+            Console.WriteLine("sum = " + sum);
+
+        }
+    }
+}
