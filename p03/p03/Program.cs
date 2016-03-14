f2e8af4d68437cc86f4ebe8bffb2db3c8e561390
Juhyung Seo <seoaro@gmail.com>
2016-03-15 02:06:17 +0900

    Problem 3 어떤 수를 소수의 곱으로만 나타내는 것을 소인수분해라 하고, 이 소수들을 그 수의 소인수라고 합니다. 예를 들면 13195의 소인수는 5, 7, 13, 29 입니다.
    600851475143의 소인수 중에서 가장 큰 수를 구하세요.

---
+++ p03/p03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Int64 j = 600851475143;
+            
+            while(true){
+                i++;
+                if (j % i == 0)
+                {
+                    j = j / i;
+                    Console.WriteLine("j = {0}, i = {1}", j, i);
+                }
+                else if (j == 1)
+                {
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
