12273c83076cf3f8d2f36f7383c4e9894530f651
Juhyung Seo <seoaro@gmail.com>
2016-03-15 02:47:11 +0900

    Problem 4 앞에서부터 읽을 때나 뒤에서부터 읽을 때나 모양이 같은 수를 대칭수(palindrome)라고 부릅니다.
    두 자리 수를 곱해 만들 수 있는 대칭수 중 가장 큰 수는 9009 (= 91 × 99) 입니다.
    
    세 자리 수를 곱해 만들 수 있는 가장 큰 대칭수는 얼마입니까?

---
+++ p04/p04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            for(int i = 0;i < 1000; i++)
+            {
+                for(int j = 0;j<1000;j++)
+                {
+                    result = i * j;
+                    if (result > 900000 && result < 1000000)
+                    {
+                        string str = result.ToString();
+                        char[] chr = str.ToCharArray();
+                        if (chr[0].Equals(chr[5]) && chr[1].Equals(chr[4]) && chr[2].Equals(chr[3]))
+                        {
+                            Console.WriteLine("{0} X {1} = {2}{3}{4}{5}{6}{7}", i, j, chr[0], chr[1], chr[2], chr[3], chr[4], chr[5]);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
